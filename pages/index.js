05dbc67dd84f97a76db48370a163f204ac1ace08
jeff mascarenhas <jeftarmascarenhas@hotmail.com>
2022-05-03 19:43:17 -0300

    refactor: UI updated

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
@@ -5,21 +6,20 @@ import useContract from "../hooks/useContract";
 import Web3 from "web3";
 
 export default function Home() {
-  const { searchNFTByAddress, nfts, loading } = useContract();
+  const { searchNFTByAddress, nfts, loading, nftInfos } = useContract();
 
-  const handleSearchSubmit = (event) => {
+  const handleSearchSubmit = async (event) => {
     event.preventDefault();
-    const address = event.target[0].value;
+    const inputSearch = event.target[0];
 
-    if (!Web3.utils.isAddress(address)) {
+    if (!Web3.utils.isAddress(inputSearch.value)) {
       alert("Address is not valid");
       return;
     }
 
-    searchNFTByAddress(address);
+    await searchNFTByAddress(inputSearch.value);
+    inputSearch.value = "";
   };
-
-  console.log(nfts.name);
 
   return (
     <>
@@ -40,30 +40,39 @@ export default function Home() {
           <form className="my-6" onSubmit={handleSearchSubmit}>
             <input
               type="search"
-              placeholder={!loading ? "Address" : "Loading NFTs..."}
+              placeholder="Your Account Address"
               className="border-0 w-full rounded-full min-h-[50px] p-4 bg-black/20 outline-none hover:shadow-lg transition-shadow ease-linear disabled:bg-gray-600 disabled:cursor-not-allowed appearance-none"
               disabled={loading}
             />
           </form>
         </header>
+
+        {!!nftInfos.name && (
+          <div className="p-6 mb-5 rounded-lg bg-purple-700">
+            <h3 className="text-lg font-semibold">{nftInfos.name}</h3>
+            <p>{nftInfos.description}</p>
+          </div>
+        )}
+
+        <div className="mb-5 text-gray-400">
+          <p>Total items: {nfts.length}</p>
+        </div>
+
         <ul className="grid gap-8 grid-cols-4">
           {nfts.length ? (
             nfts.map((nftItem) => (
               <li key={nftItem.id} className="relative mb-16">
                 <img
-                  src="https://lh3.googleusercontent.com/6iizzY2hGhxSKQZhBU5mfjpBf5GSP4vcgSQvV_pdywxHCvFf5seuVxbKv11lT7yXhhKANSFawRXtxX8V00cTNBWh2izvhHmKeNZ3GQ=w600"
+                  src={nftItem.image}
                   alt="img"
                   className="w-full object-cover rounded-md shadow-md"
                 />
                 {!!nftItem.name && (
-                  <div className="p-2 absolute -bottom-20 -left-6 ">
+                  <div className="p-2 absolute -bottom-8 -left-6 ">
                     <div className="bg-slate-800/95 p-4 rounded-lg">
                       <h2 className="text-lg font-semibold mb-2">
                         {nftItem.name}
                       </h2>
-                      {!!nftItem.description && (
-                        <h3 className="text-sm">{nftItem.description}</h3>
-                      )}
                     </div>
                   </div>
                 )}
@@ -71,7 +80,9 @@ export default function Home() {
             ))
           ) : (
             <li className="col-span-4 p-4">
-              <h3 className="text-center">NFTs not found</h3>
+              <h3 className="text-center">
+                {loading ? "Looking for NFTs..." : "Search NFTs that you own"}
+              </h3>
             </li>
           )}
         </ul>
